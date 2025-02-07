d4b5f04d1c76b8b9fe03d4b3ce1b2518bcb7af6b
d1SO <ivansokotuhin1816@gmail.com>
2025-02-07 20:53:39 +0200

    Lab3 Done

---
+++ 2nd Sem/Lab3/main.cpp
@@ -0,0 +1,46 @@
+﻿#include "main.h"
+
+class parallelepiped
+{
+	private:
+		double a;
+		double b;
+		double c;
+	public:
+		parallelepiped(double _a, double _b, double _c)
+		{
+			a = _a;
+			b = _b;
+			c = _c;
+			std::cout << "parallelepiped created" << std::endl;
+		}
+		~parallelepiped()
+		{
+			std::cout << "parallelepiped destroyed" << std::endl;
+		}
+		double GetVolume()
+		{
+			return a * b * c;
+		}
+		double GetArea()
+		{
+			return a*b;
+		}
+		double diagonal()
+		{
+			return sqrt(a*a + b*b + c*c);
+		}
+};
+
+
+int main()
+{
+	int a, b, c;
+	std::cout << "Enter a, b, c: ";
+	std::cin >> a >> b >> c;
+	parallelepiped figure(a, b, c);
+	std::cout << "Volume: " << figure.GetVolume() << std::endl;
+	std::cout << "Area: " << figure.GetArea() << std::endl;
+	std::cout << "Diagonal: " << figure.diagonal() << std::endl;
+	return 0;	
+}
