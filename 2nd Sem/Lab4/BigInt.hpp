694ade3b4d56170f61859d0d484aabc326829b48
d1SO <ivansokotuhin1816@gmail.com>
2025-02-16 19:43:59 +0200

    half-done Lab3

--- 2nd Sem/Lab4/BigInt.hpp
+++ 2nd Sem/Lab4/BigInt.hpp
@@ -4,61 +4,85 @@
 class BigInt
 {
 	public:
+
 		BigInt()
 		{
 			m_size = 0;
 			//m_inp = "";
 			m_nums = nullptr;
+			std::cout << "Empty constructor" << std::endl;
 		}
+
 		BigInt(std::string inp)
-		: m_size(inp.length()+1), m_nums(new int[m_size])
+		: m_size(inp.length()), m_nums(new int[m_size])
 		{
-			for(int i = 0; i < m_size-1; i++)
+			for(int i = 0; i < m_size; i++)
 			{
 				m_nums[i] = inp[i]-48;
 			}
 		}
-		BigInt(const BigInt& other) : BigInt(IntToStr(other.m_nums, other.m_size)) {}
-		BigInt& operator=(BigInt tmp)
+
+		BigInt(const BigInt& tmp) : BigInt(IntToStr(tmp.m_nums, tmp.m_size)) {}
+
+		BigInt& operator=(BigInt other)
 		{
-			swap(*this, tmp);
+			swap(*this, other);
 			return *this;
 		}
 
-		std::string IntToStr(int* inp, int n)
+		BigInt operator+(const BigInt& other)
 		{
-			std::string out = "";
-			for(int i = 0; i < n-1; i++)
+			int size = std::max(m_size, other.m_size);
+			if(m_nums[0]+other.m_nums[0] > 9)
 			{
-				out += std::to_string(inp[i]);
+				size++;
 			}
-			return out;
+			int res[3];
+			int mini = std::min(m_size, other.m_size);
+
+			res[size-1] = 0;
+			for(int i = 0; i < mini; i++)
+			{
+				int tmp = m_nums[m_size-i-1] + other.m_nums[other.m_size-i-1];
+				res[size-i-1] += tmp % 10;
+				res[size-i-2] = 0;
+				res[size-i-2] += tmp / 10;
+			}
+			BigInt result = IntToStr(res, size);
+			return result;
 		}
+
+		BigInt& operator+=(BigInt other)
+		{
+			*this = *this + other;
+			return *this;
+		}
+		
 		~BigInt()
 		{
 			if(m_nums != nullptr)
 				delete[] m_nums;
 		}
-		char print()
-		{
-			
-		}
-		char GetValue() const
-		{
-			return (char)m_nums[1];
-		}
-		void Setup(BigInt inp)
-		{
-			swap(*this, inp);
 
+		std::string IntToStr(int* inp, int n)
+		{
+			std::string out = "";
+			for(int i = 0; i < n; i++)
+			{
+				out += std::to_string(inp[i]);
+			}
+			return out;
 		}
-		friend std::ostream& operator << (std::ostream &os, const BigInt &bigint);
-		friend std::istream& operator >> (std::istream &in, BigInt &bigint);
-	private:
+
+		friend std::ostream& operator << (std::ostream &os, const BigInt &migint);
+		friend std::istream& operator >> (std::istream &in, BigInt &migint);
 
 		int m_size;
 		//std::string m_inp;
 		int* m_nums;
+
+	private:
+
 
 		void swap(BigInt& a, BigInt& b)
 		{
@@ -69,26 +93,28 @@ class BigInt
 
 };
 
-std::ostream& operator << (std::ostream &os, const BigInt &bigint)
-{	
-	for(int i = 0; i < bigint.m_size-1; i++)
-		os << bigint.m_nums[i];
-		os << std::endl;
+std::ostream& operator << (std::ostream &os, const BigInt &migint)
+{
+	for(int i = 0; i < migint.m_size; i++)
+	{
+		os << migint.m_nums[i];
+	}
+	os << std::endl;
 	return os;
 }
 
-std::istream& operator >> (std::istream &in, BigInt &bigint)
+std::istream& operator >> (std::istream &in, BigInt &migint)
 {	
 	std::string inp;
 	in >> inp;
-	if(bigint.m_nums == nullptr)
+	if(migint.m_nums == nullptr)
 	{
 		
-		bigint.m_size = inp.length()+1;
-		bigint.m_nums = new int[bigint.m_size];
-		for(int i = 0; i < bigint.m_size-1; i++)
+		migint.m_size = inp.length()+1;
+		migint.m_nums = new int[migint.m_size];
+		for(int i = 0; i < migint.m_size; i++)
 		{
-			bigint.m_nums[i] = inp[i]-48;
+			migint.m_nums[i] = inp[i]-48;
 		}
 	}
 	else
--- 2nd Sem/Lab4/main.cpp
+++ 2nd Sem/Lab4/main.cpp
@@ -3,24 +3,36 @@
 //#define Debug
 
 
-
+std::string IntToStr(int* inp, int n)
+{
+	std::string out = "";
+	for(int i = 0; i < n; i++)
+	{
+		out += std::to_string(inp[i]);
+	}
+	return out;
+}
 
 int main()
 {
 
 	BigInt i  = "1001204907898561904732";
 	BigInt i2 = i;
-	BigInt i3;
-
-	i3 = i2;
-
+	BigInt i3 = "99";
+	BigInt i4 = "99";
+	//i2 = i;
+	//std:: cout << (i3+i4);
+	BigInt i5 = i3+i4;
+	std::cout << i5;
+	i3 += i4;
+	std::cout<< i3;
 	//std::cin>>i3;
-	std::cout<<i3;
+	//std::cout<<i3;
 
-	
-	BigInt inp;
-	std::cin >> inp;
 
-	std::cout << inp;
+	//BigInt inp;
+	// std::cin >> inp;
+
+	// std::cout << inp;
 	return 0;
 }
