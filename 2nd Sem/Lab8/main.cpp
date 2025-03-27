888cee4e9959a671c501d9dbf0a8291496a5d389
d1SO <ivansokotuhin1816@gmail.com>
2025-03-27 14:45:00 +0200

    Lab8 Done and Lab9 added checkerboard

---
+++ 2nd Sem/Lab8/main.cpp
@@ -0,0 +1,83 @@
+﻿#include <SFML/Graphics.hpp>
+#include <math.h>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML works!");
+    window.setFramerateLimit(120);
+    short WindowSizeX = window.getSize().x;
+    short WindowSizeY = window.getSize().y;
+    sf::CircleShape shape(WindowSizeY  / 8);
+    sf::RectangleShape rect(sf::Vector2f(WindowSizeY / 4, WindowSizeY / 4));
+    sf::CircleShape octagon(WindowSizeY / 8, 8);
+    octagon.setOrigin(WindowSizeY / 8, WindowSizeY / 8);
+    rect.setOrigin(rect.getSize().x / 2, rect.getSize().y / 2);
+    shape.setOrigin(shape.getRadius(), shape.getRadius());
+
+    rect.setPosition(WindowSizeX / 4, WindowSizeY / 2);
+    shape.setPosition(WindowSizeX / 2, WindowSizeY / 2);
+    octagon.setPosition(WindowSizeX * 3 / 4, WindowSizeY / 2);
+
+    shape.setFillColor(sf::Color::Red);
+    rect.setFillColor(sf::Color::Green);
+    octagon.setFillColor(sf::Color::Blue);
+
+    bool DoMove = false;
+    float speed = 0.f;
+    while (window.isOpen())
+    {
+        speed += 0.01f;
+        if(DoMove)
+        {
+            shape.move(0.0f, -sin(speed)*2);
+            shape.rotate(1.0f);
+            rect.rotate(2.0f);
+            octagon.rotate(5.0f);
+        }
+
+        sf::Vector2 mousePos = sf::Mouse::getPosition(window);
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if(event.mouseButton.button == sf::Mouse::Left)
+                {
+                    if (shape.getGlobalBounds().contains(mousePos.x, mousePos.y))
+                    {
+                        shape.setFillColor(sf::Color::Yellow);
+                    }
+                    if (rect.getGlobalBounds().contains(mousePos.x, mousePos.y))
+                    {
+                        rect.setFillColor(sf::Color::Cyan);
+                    }
+                    if (octagon.getGlobalBounds().contains(mousePos.x, mousePos.y))
+                    {
+                        octagon.setFillColor(sf::Color::Magenta);
+                    }
+                }
+            }
+            if (event.type == sf::Event::MouseButtonReleased)
+            {    
+                shape.setFillColor(sf::Color::Red);
+                rect.setFillColor(sf::Color::Green);
+                octagon.setFillColor(sf::Color::Blue);
+            }
+            if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+            {
+                DoMove = !DoMove;
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.draw(rect);
+        window.draw(octagon);
+        window.display();
+    }
+
+    return 0;
+}
---
+++ 2nd Sem/Lab9/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <SFML/Graphics.hpp>
+#include <math.h>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1024, 1024), "Chessboard");
+    window.setFramerateLimit(120);
+    sf::RectangleShape BoardSquares[8][8];
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            BoardSquares[i][j].setSize({128.f, 128.f});
+            BoardSquares[i][j].setOrigin(64.f, 64.f);
+            BoardSquares[i][j].setFillColor((i+j) % 2 == 0 ? sf::Color(212, 191, 152) : sf::Color::Black);
+            BoardSquares[i][j].setPosition(128*i + 64, 128*j + 64);
+        }
+    }
+    short WindowSizeX = window.getSize().x;
+    short WindowSizeY = window.getSize().y;
+    sf::CircleShape shape(WindowSizeY  / 8);
+
+    sf::Event event;
+
+    
+    while(window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            if(event.type == sf::Event::Resized)
+            {
+                continue;
+            }
+        }
+        window.clear();
+        for(int i = 0; i < 8; i++)
+            for(int j = 0; j < 8; j++)
+                window.draw(BoardSquares[i][j]);
+        window.display();
+    }
+    return 0;
+}
